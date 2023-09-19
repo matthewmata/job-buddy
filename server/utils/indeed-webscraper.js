0c27a83292d04eed84cdc7839559e720ecd5397e
Matthew Mata <matthewmata1030@gmail.com>
2023-09-19 12:28:17 -0700

    checking screenshot

--- server/utils/indeed-webscraper.js
+++ server/utils/indeed-webscraper.js
@@ -10,53 +10,54 @@ const indeedWebScraper = async (url) => {
   let pageNumber = 1;
   let pageNextExists = true;
 
+  await page.screenshot({ path: "testresult.png", fullPage: true });
   // loops through each page job posts
-  while (true) {
-    // await page.waitForSelector(".cardOutline", { timeout: 20_000 });
-    // gets post info
-    const pagePosts = await page.evaluate(() => {
-      const posts = document.querySelectorAll(".job_seen_beacon");
-      return Array.from(posts).map((post) => {
-        const title = post.querySelector("span").innerText ?? "n/a";
-        const companyName =
-          post.querySelector(".companyName").innerText ?? "n/a";
-        const companyLocation =
-          post.querySelector(".companyLocation").innerText ?? "n/a";
-        const url = post.querySelector(".jobTitle > a").href ?? "n/a";
-        const salary =
-          post.querySelector(".salary-snippet-container")?.innerText ?? "n/a";
-        const description = post.querySelector(".job-snippet").innerText;
+  // while (true) {
+  //   // await page.waitForSelector(".cardOutline", { timeout: 20_000 });
+  //   // gets post info
+  //   const pagePosts = await page.evaluate(() => {
+  //     const posts = document.querySelectorAll(".job_seen_beacon");
+  //     return Array.from(posts).map((post) => {
+  //       const title = post.querySelector("span").innerText ?? "n/a";
+  //       const companyName =
+  //         post.querySelector(".companyName").innerText ?? "n/a";
+  //       const companyLocation =
+  //         post.querySelector(".companyLocation").innerText ?? "n/a";
+  //       const url = post.querySelector(".jobTitle > a").href ?? "n/a";
+  //       const salary =
+  //         post.querySelector(".salary-snippet-container")?.innerText ?? "n/a";
+  //       const description = post.querySelector(".job-snippet").innerText;
 
-        return {
-          title,
-          companyName,
-          companyLocation,
-          url,
-          salary,
-          description,
-          date: new Date().toString(),
-        };
-      });
-    });
+  //       return {
+  //         title,
+  //         companyName,
+  //         companyLocation,
+  //         url,
+  //         salary,
+  //         description,
+  //         date: new Date().toString(),
+  //       };
+  //     });
+  //   });
 
-    // adds posts
-    allPosts.push(...pagePosts);
-    // checks if we are on last page
-    pageNextExists = await page.evaluate(() => {
-      return document.querySelector('[data-testid="pagination-page-next"]');
-    });
+  //   // adds posts
+  //   allPosts.push(...pagePosts);
+  //   // checks if we are on last page
+  //   pageNextExists = await page.evaluate(() => {
+  //     return document.querySelector('[data-testid="pagination-page-next"]');
+  //   });
 
-    pageNumber++;
-    // moves to next page if exists
-    if (pageNextExists !== null) {
-      await Promise.all([
-        page.waitForNavigation(),
-        page.click(`a[aria-label="${pageNumber}"]`),
-      ]);
-    } else {
-      break;
-    }
-  }
+  //   pageNumber++;
+  //   // moves to next page if exists
+  //   if (pageNextExists !== null) {
+  //     await Promise.all([
+  //       page.waitForNavigation(),
+  //       page.click(`a[aria-label="${pageNumber}"]`),
+  //     ]);
+  //   } else {
+  //     break;
+  //   }
+  // }
 
   await browser.close();
   return allPosts;
