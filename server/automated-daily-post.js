90dba95dd9c647df0794b7c2ab9f68c22ad4fd04
Matthew Mata <matthewmata1030@gmail.com>
2023-09-19 00:09:36 -0700

    testing automated daily post

--- server/automated-daily-post.js
+++ server/automated-daily-post.js
@@ -4,7 +4,10 @@ const dotenv = require("dotenv");
 dotenv.config({path: '../.env'});
 
 // schedules post each day at "00:00"
-cron.schedule("0 0 * * *", async () => {
+const everyDay = "0 0 * * *";
+const everyMin = "*/1 * * * *";
+
+cron.schedule(everyMin, async () => {
   console.log("RUNNING!");
   console.log(`${process.env.ADDRESS}/api/job-buddy`);
   await axios.post(`${process.env.ADDRESS}/api/job-buddy`);
