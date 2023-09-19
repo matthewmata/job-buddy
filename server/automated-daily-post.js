f85f10323506b00e1535fa8022cc9bfed2b6181f
Matthew Mata <matthewmata1030@gmail.com>
2023-09-19 00:17:05 -0700

    added port

--- server/automated-daily-post.js
+++ server/automated-daily-post.js
@@ -9,8 +9,8 @@ const everyMin = "*/1 * * * *";
 
 cron.schedule(everyMin, async () => {
   console.log("RUNNING!");
-  console.log(`${process.env.ADDRESS}/api/job-buddy`);
-  await axios.post(`${process.env.ADDRESS}/api/job-buddy`);
+  console.log(`${process.env.ADDRESS}:${process.env.PORT}/api/job-buddy`);
+  await axios.post(`${process.env.ADDRESS}:${process.env.PORT}/api/job-buddy`);
   try {
     console.log("successful post");
   } catch {
