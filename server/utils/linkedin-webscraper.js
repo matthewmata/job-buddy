98a8ad6d91505bab438510c3a6e7d6a2a5a43a25
Matthew Mata <matthewmata1030@gmail.com>
2023-09-20 12:01:11 -0700

    finished linked in webscraper

--- server/utils/linkedin-webscraper.js
+++ server/utils/linkedin-webscraper.js
@@ -2,16 +2,45 @@ const puppeteer = require("puppeteer");
 
 const linkedinWebScraper = async (url) => {
   // browser set up
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
   await page.goto(url);
-  await page.screenshot({ path: "testresult.png", fullPage: true });
+
+  const allPosts = [];
+
+  await page.waitForSelector(".jobs-search__results-list", {
+    timeout: 10_000,
+  });
+  // gets post info
+  const pagePosts = await page.evaluate(() => {
+    const posts = document.querySelectorAll(".jobs-search__results-list > li");
+    return Array.from(posts).map((post) => {
+      const title =
+        post.querySelector(".base-search-card__title").innerText ?? "n/a";
+      const companyName =
+        post.querySelector(".base-search-card__subtitle").innerText ?? "n/a";
+      const companyLocation =
+        post.querySelector(".job-search-card__location").innerText ?? "n/a";
+      const url = post.querySelector("a").href ?? "n/a";
+      const salary =
+        post.querySelector(".job-search-card__salary-info")?.innerText ?? "n/a";
+      return {
+        title,
+        companyName,
+        companyLocation,
+        url,
+        salary,
+        description: "n/a",
+        date: new Date().toString(),
+      };
+    });
+  });
+
+  // adds posts
+  allPosts.push(...pagePosts);
+  
   await browser.close();
   return allPosts;
 };
-
-linkedinWebScraper(
-  "https://www.linkedin.com/jobs/search/?currentJobId=3724494736&distance=25&f_TPR=r86400&f_WT=2&geoId=103644278&keywords=react%20developer%20-sr%20-senior%20-C%23%20-staff&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true"
-);
 
 module.exports = linkedinWebScraper;
