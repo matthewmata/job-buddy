709ea30ba64aae44104643568659d2902eb66eec
Matthew Mata <matthewmata1030@gmail.com>
2023-09-20 14:23:24 -0700

    minor changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Job } from "./models/models";
 
-const App = () => {
+const App: React.FC = () => {
   const [jobs, setJobs] = useState<Job[]>([]);
   const [error, setError] = useState<Boolean>(false);
+  
+  const serverUrl = process.env.REACT_APP_SERVER_URL!;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response: Job[] = await axios.get("http://localhost:6000/api/job-buddy");
+        const response: Job[] = await axios.get(serverUrl);
         console.log(response);
         setJobs(response);
       } catch (err) {
